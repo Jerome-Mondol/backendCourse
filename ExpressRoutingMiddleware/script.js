ab7f6ee84ea735fa6d26e835ffa15a488fb4df9b
Jerome Mondol <jeromemondol11@gmail.com>
2025-04-24 23:58:46 +0600

    Express middleware

--- ExpressRoutingMiddleware/script.js
+++ ExpressRoutingMiddleware/script.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("I am middleware!!!!!!!!!!")
+
+    next()
+})
+
+
 app.get("/", (req, res) => {
     res.send("Hello from Jerome!!")
 })
