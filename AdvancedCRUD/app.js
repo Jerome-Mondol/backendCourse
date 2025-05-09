e5155682859625c359ae0abadcc280d8278c1061
Jerome Mondol <jeromemondol11@gmail.com>
2025-05-09 23:09:17 +0600

    simple mogodb practice project

---
+++ AdvancedCRUD/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require('./models/user');
+
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/read", async (req, res) => {
+    let allUsers = await userModel.find()
+    res.render("read", { allUsers });
+})
+
+app.get("/delete/:id", async (req, res) => {
+    let deletedUser = await userModel.findOneAndDelete({ _id: req.params.id})
+    res.redirect("/read");
+})
+
+app.post("/create", async (req, res) => {
+    let { image, email, name } = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+
+    res.redirect("/read")
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
