da67d018ef8a0d275699606994cfdd062cdba066
Jerome Mondol <jeromemondol11@gmail.com>
2025-05-12 23:25:33 +0600

    jwt

--- bcrypt/app.js
+++ bcrypt/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -18,6 +23,18 @@ app.get('/compare', (req, res) => {
     })
 })
 
+app.get("/jwt", (req, res) => {
+    let token = jwt.sign({email: "jerome@example.com"}, "secret")
+    res.cookie("token", token)
+    res.send("Token Created");
+    console.log(token)
+})
+
+app.get("/verify", (req, res) => {
+    res.send("Verifying Token");
+    let data = jwt.verify(req.cookies.token, "secret")
+    console.log(data)
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
